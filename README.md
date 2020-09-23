2cbe91b26f5218856286e572e4ac7e0ae19002c8
Tiya <teeeya@gmail.com>
2020-09-23 12:19:31 +0100

    A simple hello world app in flutter

---
+++ README.md
@@ -0,0 +1,9 @@
+# helloworld
+
+This is the HelloWorld App in Flutter.
+
+More information can be found here: https://medium.com/flutter-community/the-hello-world-tutorial-to-get-you-started-2d7acd98c89d.
+
+Happy Coding! :) 
+
+
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+main() => runApp(MyApp());
+class MyApp extends StatelessWidget {
+@override
+ Widget build(BuildContext context) => MaterialApp(
+   home: Scaffold(
+     appBar: AppBar(title: Text('Hello World')),
+     body: Text('Hello, World!'),
+    )
+  );
+}
